4f80145c655918474ffdfcc424ad65e208f841a8
pratheek <pratheekmanimaran@gmail.com>
2023-02-19 18:57:10 +0530

    Fourth Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,14 @@ import styled from "styled-components";
 
 const FooterBar = styled.div`
     background-color: gray;
+    background: rgba(204, 204, 204, 0.5);
     text-align: center;
     margin-top: 100px;
-    position: relative;
-    top: 368px;
+    position: fixed;
+    z-index: 0;
+    width: 100%;
+    bottom: 0;
+    left: 0;
     padding: 8px;
 `;
 
